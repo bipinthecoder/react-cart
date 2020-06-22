1b0f5ba601fda12523a246384a59fae299a15759
Bipin Babu <bipinbabu@live.com>
2020-06-22 10:36:35 +0530

    Adding initial setup

---
+++ react-cart-main/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import "./Components/FontAwesomeIcons";
+
+function App() {
+  return (
+    <div className="App">
+      Landing Page
+    </div>
+  );
+}
+
+export default App;
